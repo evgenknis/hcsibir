322bf3daafc24428343dc0e55048c4533b13c0b0
e.knis <e.knis@uk-rnd-48.2gis.local>
2016-04-19 13:58:23 +0600

    message1

--- config/web.php
+++ config/web.php
@@ -9,7 +9,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'knismaksim1',
+            'cookieValidationKey' => 'knismaksim',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
